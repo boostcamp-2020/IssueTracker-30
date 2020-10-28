c32b19427bab1b1fe21ccc1e70b521aebaec94fa
SSH1997 <akasiassh@naver.com>
2020-10-28 17:37:05 +0900

    FEAT: App render
    - 모드에 따라서 app.jsx 렌더링 다르게 함

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import NavBar from "./components/nav-bar.jsx";
 import MainSection from "./main-section.jsx";
 import Footer from "./components/footer.jsx";
 
+const loginCheck = async () => {
+    if (document.cookie.split("=")[0] === "user") {
+        const mode = await axios({
+            method: "POST",
+            url: "http://localhost:3000/signIn/auth",
+            withCredentials: true,
+        })
+            .then((res) => {
+                if(res.data.message === "success"){
+                    return "main";
+                } else {
+                    return "login";
+                }
+            });
+        return mode;
+    } else {
+        return "login";
+    }
+}
+
 const App = () => {
+    const [mode, setMode] = useState("login");
+
+    useEffect(() => {
+        loginCheck().then((res) => {
+            setMode(res);
+        });
+    }, []);
+
     return (
         <>
             <NavBar />
-            <MainSection />
+            <MainSection mode={mode}/>
             <Footer />
         </>
     );
