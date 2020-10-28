fff66f966b6e6089d0e920c674ffe55014423a9c
dh00n <0930dhs@gmail.com>
2020-10-28 17:15:47 +0900

    feat: signup 라우터 추가

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import hello from "./hello";
 import signIn from "./signIn"
-
+import signUp from "./signUp"
 const router = express.Router();
 
 router.use("/hello", hello);
 router.use("/signIn", signIn);
+router.use("/signUp", signUp);
 
 export default router;
