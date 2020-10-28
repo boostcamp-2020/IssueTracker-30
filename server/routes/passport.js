a4a6c2b729a989287bfd759d01d4e764b21c85d9
kas136 <kas136@naver.com>
2020-10-28 11:05:56 +0900

    FEAT: 서버 db와 연결
    서버 db와 연결하여 select문 실행

--- server/routes/passport.js
+++ server/routes/passport.js
@@ -1,13 +1,20 @@
 const passport = require("passport");
 const { Strategy: JwtStrategy } = require("passport-jwt");
 const { Strategy: LocalStrategy } = require("passport-local");
-const DB = require('./db.json');
+const DB = require("./db.json");
+const pool = require("./connection");
 
 module.exports = () => {
     passport.use('local', new LocalStrategy({
         usernameField: 'userId',
         passwordField: 'userPw'
-    }, (id, pw, done) => {
+    }, async(id, pw, done) => {
+        const connection = await pool.getConnection();
+        const [rows] = await connection.query('select * from user;');
+        console.log(rows);
+        connection.release();
+
+
         if(id === DB.id && pw === DB.password) {
             return done(null, id, {message: "success"});
         }
