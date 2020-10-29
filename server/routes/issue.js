e0741c31b361d3f23c55f36b0ad79c44c5c1d134
kas136 <kas136@naver.com>
2020-10-29 16:35:32 +0900

    Feat: issue 라우터 생성
    issue 라우터 생성
    issue 목록 불어오기
    issue 추가기능 구현
    issue 수정 기능 구현

--- server/routes/issue.js
+++ server/routes/issue.js
@@ -4,27 +4,61 @@ import query from "../db/query";
 
 const router = express.Router();
 
-router.get("/", async(req, res) => {
+router.post("/getIssue", async(req, res) => {
+    const userId = req.body.userId;
     const connection = await pool.getConnection();
-    const [rows] = await connection.query(query.getIssue);
+    const [rows] = await connection.query(query.getIssue, [userId]);
     console.log(rows);
     res.json(rows);
 });
 
-/*
-router.post("/", (req, res) => {
-    console.log(req.body);
-    res.json({
-        message: "Hello! I'm from Express Server!",
-    });
+router.post("/insertIssue", async(req, res) => {
+    const issue = {
+        title: req.body.title,
+        writingTime: req.body.writingTime,
+        status: req.body.status,
+        milestoneId: req.body.milestoneId,
+        content: req.body.content,
+        labelId: req.body.labelId
+    };
+
+    const connection = await pool.getConnection();
+    const [rows1] = await connection.query(query.insertIssue, ["123123", issue.title, issue.writingTime, issue.status, issue.milestoneId, issue.content, issue.labelId]);
+
+    if(issue.labelId != null) {
+        const [rows2] = await connection.query(query.insertLabelIssueRelation, [rows1.insertId, issue.labelId]);
+        if(rows2.affectedRows <= 0) {
+            res.json({messages: "Error"});
+        }
+    }
+
+    if(rows1.affectedRows > 0) {
+        res.json({message: "success"});
+    }
+    else {
+        res.json({messages: "Error"});
+    }
 });
 
-router.put("/", (req, res) => {
-    console.log(req.body);
-    res.json({
-        message: "Hello! I'm from Express Server!",
-    });
+router.put("/", async(req, res) => {
+    const issue = {
+        //userId 필요
+        issueId: req.body.issueId,
+        title: req.body.title,
+        writingTime: req.body.writingTime,
+        status: req.body.status,
+        milestoneId: req.body.milestoneId,
+        content: req.body.content
+    };
+
+    const connection = await pool.getConnection();
+    const [rows] = await connection.query(query.updateIssue, [issue.title, issue.writingTime, issue.status, issue.milestoneId, issue.content, issue.issueId]);
+    if(rows.affectedRows > 0) {
+        res.json({message: "success"});
+    }
+    else {
+        res.json({messages: "Error"});
+    }
 });
-*/
 
 export default router;
