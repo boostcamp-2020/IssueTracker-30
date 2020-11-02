3d2a0d2b27cb5927ede3cd5dc811a31b47075183
kas136 <kas136@naver.com>
2020-11-02 18:58:37 +0900

    Feat: getUser api 추가
    유저 전체 목록을 가져오는 api 기능 추가

---
+++ server/routes/user.js
@@ -0,0 +1,14 @@
+import express from "express";
+import isLoggedIn from "./auth";
+import pool from "../db/connection";
+import query from "../db/query";
+
+const router = express.Router();
+
+router.get("/", isLoggedIn, async(req, res) => {
+    const connection = await pool.getConnection();
+    const [rows] = await connection.query(query.getUser);
+    res.json(rows);
+});
+
+export default router;
