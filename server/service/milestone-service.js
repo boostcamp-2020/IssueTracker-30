d35a82aaabe5145cceb570791b9b2e0334c648cb
kas136 <kas136@naver.com>
2020-11-03 16:53:58 +0900

    Fix: connection error 수정
    getConnection 삭제

--- server/service/milestone-service.js
+++ server/service/milestone-service.js
@@ -1,9 +1,8 @@
-import pool from "../db/connection";
+import connection from "../db/connection";
 import query from "../db/query";
 
 const MilestoneService = {
     getMilestone: async (req, res) => {
-        const connection = await pool.getConnection();
         const [rows] = await connection.query(query.getMilestone);
         res.json(rows);
     }
