bac5a9f66902d1089dd13fb480befc79a61b3979
Jeong-Hoon Wi <wiii@Jeong-Hoons-MacBook-Pro.local>
2020-11-05 15:28:16 +0900

    STYLE: linted by Prettier-ESLint, remove `log`

--- client/src/issues/issue-item-banner.jsx
+++ client/src/issues/issue-item-banner.jsx
@@ -43,31 +43,20 @@ const StyledBannerInfo = styled.p`
 let count = 0;
 let total;
 
-// color: "#FF0000"
-// content: "bug"
-// issueId: 1
-// issueTitle: "목록 보기 구현"
-// labelId: 2
-// milestoneId: 1
-// milestoneTitle: "스프린트2"
-// status: 1
-// userId: "123123"
-// writingTime: "2020-10-28T15:00:00.000Z"
 const IssueTitle = (props) => {
     const [checked, setChecked] = useState(false);
     const openOrClosed = props.status === 1 ? "opened" : "closed";
 
     const timeNow = Date.now();
     const updatedTimeBefore = new Date(
-        timeNow - new Date(props.writingTime),
+        timeNow - new Date(props.writingTime)
     ).getDate();
 
     useEffect(() => {
         if (props.checked) {
             setChecked(props.checked);
             count = props.count;
-        }
-        else {
+        } else {
             if (count == props.count || count == 0) {
                 setChecked(props.checked);
                 count = 0;
@@ -77,7 +66,7 @@ const IssueTitle = (props) => {
 
     const checkedFunc = () => {
         return checked;
-    }
+    };
 
     const setCheckFunc = () => {
         if (checked) {
@@ -85,8 +74,7 @@ const IssueTitle = (props) => {
             props.excludeIssueFunc(props.issueId);
             props.func2(false);
             count--;
-        }
-        else {
+        } else {
             count++;
             props.addIssueFunc(props.issueId);
             if (count == props.count) {
@@ -95,12 +83,15 @@ const IssueTitle = (props) => {
         }
         props.selectedFunc(count);
         setChecked(!checked);
-    }
+    };
 
     return (
         <StyledBannersListDiv>
             <StyledBannerCheckBoxDiv>
-                <StyledBannerCheckBoxInput checked={checkedFunc()} onChange={setCheckFunc} />
+                <StyledBannerCheckBoxInput
+                    checked={checkedFunc()}
+                    onChange={setCheckFunc}
+                />
             </StyledBannerCheckBoxDiv>
 
             <StyledBannerOpenClosedDiv>
@@ -109,6 +100,7 @@ const IssueTitle = (props) => {
 
             <StyledBannerTextDiv>
                 <StyledBannerTitle>{props.issueTitle}</StyledBannerTitle>
+                {console.log(props.labelInfo)}
                 <StyledBannerInfo>
                     #{props.issueId} by {props.userId} was {openOrClosed}{" "}
                     {updatedTimeBefore} days ago
