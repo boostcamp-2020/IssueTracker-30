4af82279ddec7c3b6ec4751980be36b222f5b42c
Jeong-Hoon Wi <wiii@Jeong-Hoons-MacBook-Pro.local>
2020-11-10 13:27:49 +0900

    REFACTOR: component 분리에 따른 import 위치 변경

--- client/src/labels/labels-list.jsx
+++ client/src/labels/labels-list.jsx
@@ -2,22 +2,22 @@ import React from "react";
 import styled from "styled-components";
 
 import SearchHeader from "./labels-search-header.jsx";
-import SearchResults from "./search-list.jsx";
+import SearchResults from "../components/search-list.jsx";
 
 const StyledLabelList = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
 `;
 
 const LabelsList = () => {
-  return (
-    <StyledLabelList>
-      <SearchHeader />
-      <SearchResults />
-    </StyledLabelList>
-  );
+    return (
+        <StyledLabelList>
+            <SearchHeader />
+            <SearchResults />
+        </StyledLabelList>
+    );
 };
 
 export default LabelsList;
